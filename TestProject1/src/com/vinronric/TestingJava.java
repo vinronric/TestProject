5507c19dc39f01a5ad8150ff8aac8caa9c4a730f
vinronric <33157881+vinronric@users.noreply.github.com>
2018-06-07 13:43:34 -0400

    added TestingJava.java class

---
+++ TestProject1/src/com/vinronric/TestingJava.java
@@ -0,0 +1,10 @@
+package com.vinronric;
+
+public class TestingJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hello World!" );
+	}
+
+}
